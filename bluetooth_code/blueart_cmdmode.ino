dcaf2c50c15d8d80ad6ae09964134e84ede259a5
Micha <74900539+michafvdw@users.noreply.github.com>
2021-03-29 10:05:44 +0200

    Update blueart_cmdmode.ino

--- bluetooth_code/blueart_cmdmode.ino
+++ bluetooth_code/blueart_cmdmode.ino
@@ -12,7 +12,7 @@
  any redistribution
 *********************************************************************/
 
-#include <Arduino.h>
+#include <Arduino.h> 
 #include <SPI.h>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
